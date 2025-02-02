8605d6aab4de1943bdf35ee05808fc9bf82daf00
Arley Souto <macx15@hotmail.com>
2025-02-02 18:12:49 -0300

    feat: autenticação com firebase

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Remaider' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Remaider
+  pod 'Firebase/Auth' 
+
+end
--- Remaider/Sources/Features/LogginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
+++ Remaider/Sources/Features/LogginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -6,9 +6,19 @@
 //
 
 import Foundation
+import Firebase
 
 class LoginBottomSheetViewModel {
     func doAuth(userNameLogin: String, password: String){
-        
+        Auth.auth().signIn(withEmail: userNameLogin, password: password) { [weak self] AuthDataResult, error in
+            
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }else{
+                print(AuthDataResult?.user ?? "User not found")
+            }
+            
+        }
     }
 }
